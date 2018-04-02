601da34860d44a05eb1647230411a55107f70582
github username <github email address>
2018-04-02 15:50:12 -0400

    haven't saved in a bit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
-    render json: @players
+    render json: @players, include: ['contracts.seasons', 'contracts.dead_seasons']
   end
 
   def show
